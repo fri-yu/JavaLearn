0a18269636f3327bf17c05d4bbc5a416db147c70
fri-yu <18768860215@163.com>
2015-09-07 17:16:14 +0800

    数组Demo

---
+++ Test/ArrayDemo.java
@@ -0,0 +1,26 @@
+class ArrayDemo
+{
+public static void main(String[] args)
+{
+int a[]=new int[2];
+int[] a2=new int[2];
+int a3[]={5,6};
+int[] a4={7,8};
+a[0]=1;
+a[1]=2;
+a2[0]=3;
+a2[1]=4;
+String ad[]=new String[2];
+String[] ad2=new String[2];
+System.out.println(a[0]);
+System.out.println(a[1]);
+System.out.println(a2[0]);
+System.out.println(a2[1]);
+System.out.println(a3[0]);
+System.out.println(a3[1]);
+System.out.println(a4[0]);
+System.out.println(a4[1]);
+int [][]a5={{1,2}};
+int a6[][]={{1,2}};
+}
+}
