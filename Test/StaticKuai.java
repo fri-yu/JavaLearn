52be7217c60aaec14da615ed5d4f3ae9d4a374eb
fri-yu <18768860215@163.com>
2015-09-08 17:31:42 +0800

    静态代码块，构造代码块

---
+++ Test/StaticKuai.java
@@ -0,0 +1,15 @@
+class StaticKuai 
+{
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+	
+	{
+		System.out.println("构造代码块");
+	}
+	static{
+		System.out.println("静态代码块");
+	}
+}
