3850522034664b836fcc20f3a489a5be4c767268
fri-yu <18768860215@163.com>
2015-09-08 08:45:37 +0800

    main 方法

---
+++ Test/MainMethod.java
@@ -0,0 +1,12 @@
+class  MainMethod
+{
+	public static int main(String args) 
+	{
+		System.out.println("Hello World!");
+		return 1;
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+}
