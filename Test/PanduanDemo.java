f87cfde96b9a90f4c4bc91125c89bf72f596c6f4
fri-yu <18768860215@163.com>
2015-09-11 20:00:10 +0800

    等号与equals

---
+++ Test/PanduanDemo.java
@@ -0,0 +1,10 @@
+class PanduanDemo 
+{
+	public static void main(String[] args) 
+	{
+		Object o1=new Object();
+		Object o2=new Object();
+		System.out.println("o1==o2?" + (o1==o2 ));//f 默认比较的是地址
+		System.out.println("o1 equals o2?"+o1.equals(o2));//t Object 基类的默认equals方法是比较内容
+	}
+}
