049f86fad061421b839c536af7b7950f569ea4ea
fri-yu <18768860215@163.com>
2015-09-12 15:04:54 +0800

    ==与equals注解

--- Test/PanduanDemo.java
+++ Test/PanduanDemo.java
@@ -4,7 +4,7 @@ class PanduanDemo
 	{
 		Object o1=new Object();
 		Object o2=new Object();
-		System.out.println("o1==o2?" + (o1==o2 ));//f 默认比较的是地址
-		System.out.println("o1 equals o2?"+o1.equals(o2));//t Object 基类的默认equals方法是比较内容
+		System.out.println("o1==o2?--" + (o1==o2 ));//f 默认比较的是地址
+		System.out.println("o1 equals o2?--"+o1.equals(o2));//f Object 基类的默认equals方法与“==”一致，都是比较地址
 	}
 }
