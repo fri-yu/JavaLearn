4c1d835e42125742c250de9b59db00835b6974a2
fri-yu <18768860215@163.com>
2015-09-22 09:02:48 +0800

    多线程，不能多次start

--- Test/ThreadStart.java
+++ Test/ThreadStart.java
@@ -3,9 +3,18 @@ class ThreadStart
 	public static void main(String[] args){
 		TDemo t=new TDemo();
 		t.start();
-/*try{	Thread.sleep(1000);}catch(Exception e){}
-System.out.println(t.getState() );*/
-		//t.start();//多次start会抛异常
+		while(true)
+		{
+/*try{	Thread.sleep(1000);}catch(Exception e){}*/
+/*if(!t.isAlive())
+			{
+System.out.println(t.getState() );
+System.out.println(t.isAlive());
+break;
+			}
+		}
+		*/
+		//t.start();//多次start会抛异常 CSDN上的解释，线程属于一次性资源，使用完后就被回收，无法重新开启，需要使用new来重新创建然后start
 		System.out.println("Hello World!");
 	}
 }
