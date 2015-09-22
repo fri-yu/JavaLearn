74af2dffb9e713b62c8f1ea732d997e8db7afc36
fri-yu <18768860215@163.com>
2015-09-22 09:01:36 +0800

    键盘输入流

---
+++ Test/ScannerDemo.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class ScannerDemo 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+		System.out.println(str);
+	}
+}
