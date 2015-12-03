3166c196f2c15c956efae0d138a108630c278bb0
fri-yu <18768860215@163.com>
2015-12-03 20:50:24 +0800

    试题列表功能类实现

--- ServerletFilter/src/com/demo/bussiness/QuestionList.java
+++ ServerletFilter/src/com/demo/bussiness/QuestionList.java
@@ -1,12 +1,24 @@
 package com.demo.bussiness;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.ListModel;
+
+import com.demo.domain.QuestionChoice;
+import com.demo.domain.QuestionCompletion;
+import com.demo.domain.QuestionShortanswer;
+import com.demo.domain.QuestionTrueorfalse;
+import com.demo.service.BaseService;
+import com.demo.util.EQuestionType;
+import com.demo.util.PagerUtil;
+import com.demo.viewModel.QuestionListModel;
 
 public class QuestionList extends HttpServlet {
 
@@ -16,7 +28,8 @@ public class QuestionList extends HttpServlet {
 		// TODO Auto-generated method stub
 		// super.doGet(req, resp);
 		req.setAttribute("model", initModel(req));
-		RequestDispatcher dispatcher=req.getRequestDispatcher("/question/questionList.jsp");
+		RequestDispatcher dispatcher = req
+				.getRequestDispatcher("/question/questionList.jsp");
 		dispatcher.forward(req, resp);
 	}
 
@@ -29,7 +42,46 @@ public class QuestionList extends HttpServlet {
 
 	private Object initModel(HttpServletRequest req) {
 		// TODO Auto-generated method stub
-		return null;
+		QuestionListModel model = new QuestionListModel();
+		String queryString = "";
+		String questionType = req.getParameter("questionType");
+		model.setSelectQuestionType(questionType);
+		queryString = "questionType=" + questionType;
+		int pageCount = 0;
+		int pageSize = req.getParameter("pageSize") == null ? 5 : Integer
+				.parseInt(req.getParameter("pageSize").trim());// 默认每页10条
+		int currentPage = req.getParameter("currentPage") == null ? 1 : Integer
+				.parseInt(req.getParameter("currentPage").trim());// 默认第一页
+		if (questionType != null) {
+			if (questionType.equals(EQuestionType.QChoice.getKey())) {
+				List<QuestionChoice> cList = new ArrayList<QuestionChoice>();
+				pageCount = BaseService.getList(pageSize, currentPage, cList,
+						" QuestionChoice ", "", null);
+				model.setqCList(cList);
+			} else if (questionType.equals(EQuestionType.QCompletion.getKey())) {
+				List<QuestionCompletion> cList = new ArrayList<QuestionCompletion>();
+				pageCount = BaseService.getList(pageSize, currentPage, cList,
+						" QuestionCompletion ", "", null);
+				model.setqComList(cList);
+			} else if (questionType.equals(EQuestionType.QTrueOrFalse.getKey())) {
+				List<QuestionTrueorfalse> cList = new ArrayList<QuestionTrueorfalse>();
+				pageCount = BaseService.getList(pageSize, currentPage, cList,
+						" QuestionTrueorfalse ", "", null);
+				model.setqTList(cList);
+			} else if (questionType.equals(EQuestionType.QSAQ.getKey())) {
+				List<QuestionShortanswer> cList = new ArrayList<QuestionShortanswer>();
+				pageCount = BaseService.getList(pageSize, currentPage, cList,
+						" QuestionShortanswer ", "", null);
+				//model.setsAQList(cList);
+			}
+		} else {
+			model.setSelectQuestionType("Choice");
+			queryString = "questionType=Choice";
+		}
+		model.setPageCount(pageCount);
+		model.setPageLinkDic(PagerUtil.getPagerMap(pageCount, currentPage,
+				pageSize, queryString));
+		return model;
 	}
 
 }
