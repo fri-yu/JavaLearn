9a673854415967581a977c2b544aacc76ca7463c
fri-yu <18768860215@163.com>
2015-12-08 20:24:56 +0800

    1111

--- ServerletFilter/src/com/demo/service/PaperService.java
+++ ServerletFilter/src/com/demo/service/PaperService.java
@@ -1,15 +1,5 @@
 package com.demo.service;
 
-import java.io.Serializable;
-import java.util.List;
-
-import org.hibernate.Query;
-import org.hibernate.Session;
-import org.hibernate.type.ClassType;
-
-import com.demo.domain.User;
-import com.demo.util.HibernateUtil;
-
 public class PaperService extends BaseService {
 
 }
